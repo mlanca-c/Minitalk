6b0cf8b15df640be0d2113bb85315e61ac407e9d
mlanca-c <mlanca-c@student.42lisboa.com>
2021-06-25 16:01:32 +0100

    Update minitalk.h added structure t_message.

--- minitalk.h
+++ minitalk.h
@@ -6,14 +6,24 @@
 /*   By: mlanca-c <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/22 12:53:25 by mlanca-c          #+#    #+#             */
-/*   Updated: 2021/06/23 12:57:33 by mlanca-c         ###   ########.fr       */
+/*   Updated: 2021/06/25 12:51:42 by mlanca-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_H
 # define MINITALK_H
 
-# include <sys/types.h>
+# include <unistd.h>
+# include <signal.h>
+# include <stdlib.h>
 # include "ft_printf.h"
+
+# define SIZE 1
+
+typedef struct s_message
+{
+	char	str[SIZE + 1];
+	int		bit;
+}	t_message;
 
 #endif
