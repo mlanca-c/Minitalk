77a6fee9a49550e6a6257d33f2ce6278a7414898
MLSTT <marialsottomayor@gmail.com>
2021-06-28 20:03:07 +0100

    Update work Still needs to fix errors but logic is all there. Need commentary too.

--- minitalk.h
+++ minitalk.h
@@ -5,25 +5,16 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mlanca-c <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/06/22 12:53:25 by mlanca-c          #+#    #+#             */
-/*   Updated: 2021/06/25 12:51:42 by mlanca-c         ###   ########.fr       */
+/*   Created: 2021/06/28 11:35:11 by mlanca-c          #+#    #+#             */
+/*   Updated: 2021/06/28 18:01:43 by mlanca-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_H
 # define MINITALK_H
 
-# include <unistd.h>
 # include <signal.h>
-# include <stdlib.h>
-# include "ft_printf.h"
-
-# define SIZE 1
-
-typedef struct s_message
-{
-	char	str[SIZE + 1];
-	int		bit;
-}	t_message;
-
+# include "libft.h"
+# include <stdio.h>
+void	install_handler(struct sigaction *sa_signal);
 #endif
